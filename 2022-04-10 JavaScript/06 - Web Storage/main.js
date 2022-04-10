45ac33f89ec131601b5dcf27ea43dd22aeb0f3a5
barsheshet <guybarsheshet1@gmail.com>
2022-04-10 21:17:57 +0300

    From 10/04

--- 2022-04-10 JavaScript/06 - Web Storage/main.js
+++ 2022-04-10 JavaScript/06 - Web Storage/main.js
@@ -2,12 +2,40 @@ function saveCookie() {
   const fullNameBox = document.querySelector("#fullNameBox");
   const fullName = fullNameBox.value;
 
-  // Non Persistent cookie
-  //document.cookie = `fullName=${fullName}`;
+  // // Non Persistent cookie
+  // document.cookie = `fullName=${fullName}`;
 
+  // Persistent cookie
   const now = new Date();
-  now.setSeconds(now.getSeconds() + 3);
+  now.setMonth(now.getMonth() + 3);
   document.cookie = `fullName=${fullName}; expires=${now}`;
+}
+
+function loadCookie() {
+  alert(document.cookie);
+}
+
+function saveSession() {
+  const fullNameBox = document.querySelector("#fullNameBox");
+  const fullName = fullNameBox.value;
+
+  sessionStorage.setItem("userFullName", fullName);
+}
+
+function loadSession() {
+  const fullName = sessionStorage.getItem("userFullName");
+  alert(fullName);
+}
+
+function saveLocal() {
+  const fullNameBox = document.querySelector("#fullNameBox");
+  const fullName = fullNameBox.value;
+  localStorage.setItem("userFullName", fullName);
+}
+
+function loadLocal() {
+  const fullName = localStorage.getItem("userFullName");
+  alert(fullName);
 }
 
 function onWindowLoad() {
@@ -15,6 +43,16 @@ function onWindowLoad() {
   const loadCookieBtn = document.querySelector("#loadCookie");
   saveCookieBtn.onclick = saveCookie;
   loadCookieBtn.onclick = loadCookie;
+
+  const saveSessionBtn = document.querySelector("#saveSession");
+  const loadSessionBtn = document.querySelector("#loadSession");
+  saveSessionBtn.onclick = saveSession;
+  loadSessionBtn.onclick = loadSession;
+
+  const saveLocalBtn = document.querySelector("#saveLocal");
+  const loadLocalBtn = document.querySelector("#loadLocal");
+  saveLocalBtn.onclick = saveLocal;
+  loadLocalBtn.onclick = loadLocal;
 }
 
 window.onload = onWindowLoad;
