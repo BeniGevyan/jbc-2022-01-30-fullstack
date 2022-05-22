bc1440333114fa41a53e0023a24bd4f77df99aa0
barsheshet <guybarsheshet1@gmail.com>
2022-05-22 19:30:41 +0300

    from 22/05

---
+++ 2022-05-22 Advanced JavaScript/01 - AJAX - XMLHttpRequest/main.js
@@ -0,0 +1,30 @@
+function getTodos() {
+  const request = new XMLHttpRequest();
+
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        const todoList = JSON.parse(request.responseText);
+
+        const todoUl = document.querySelector('#todosUl');
+
+        todoUl.innerHTML = '';
+
+        for (let t of todoList) {
+          todoUl.innerHTML += `<li>${t.title}</li>`;
+        }
+      }
+    }
+  };
+
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+
+  request.send();
+}
+
+function onWindowLoad() {
+  const btn1 = document.querySelector('#btn1');
+  btn1.onclick = getTodos;
+}
+
+window.onload = onWindowLoad;
---
+++ 2022-05-22 Advanced JavaScript/Class Exercise/README.md
@@ -0,0 +1,7 @@
+# Class Exercise
+
+1. Create a webpage with a button with the text "Get Users".  
+   When the user clicks on the button, use `XMLHttpRequest` request to get the list of users from this URL: https://jsonplaceholder.typicode.com/users.  
+   Display them in a table, for each user, display name, username, email, and city.
+
+2. Use `fetch` to get the list of todos (the first example).
