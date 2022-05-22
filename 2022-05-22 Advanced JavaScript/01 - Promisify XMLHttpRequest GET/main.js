293718f979bf249367ca10cf49b0470b4c264366
barsheshet <guybarsheshet1@gmail.com>
2022-05-22 20:04:32 +0300

    from 22/05

--- 2022-05-22 Advanced JavaScript/01 - Promisify XMLHttpRequest GET/main.js
+++ 2022-05-22 Advanced JavaScript/01 - Promisify XMLHttpRequest GET/main.js
@@ -1,40 +1,58 @@
-function getUsers() {
-  const request = new XMLHttpRequest();
+function displayUsers(usersList) {
+  const usersTable = document.querySelector('#usersTable');
 
-  request.onreadystatechange = () => {
-    if (request.readyState === 4) {
-      if (request.status === 200) {
-        const usersList = JSON.parse(request.responseText);
+  usersTable.innerHTML = `<tr>
+    <th>Name</th>
+    <th>Username</th>
+    <th>Email</th>
+    <th>City</th>
+  </tr>`;
 
-        const usersTable = document.querySelector('#usersTable');
+  for (const u of usersList) {
+    usersTable.innerHTML += `<tr>
+      <td>${u.name}</td>
+      <td>${u.username}</td>
+      <td>${u.email}</td>
+      <td>${u.address.city}</td>
+    </tr>`;
+  }
+}
 
-        usersTable.innerHTML = `<tr>
-          <th>Name</th>
-          <th>Username</th>
-          <th>Email</th>
-          <th>City</th>
-        </tr>`;
+// general ajax GET request
+function getJsonAsync(url) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-        for (const u of usersList) {
-          usersTable.innerHTML += `<tr>
-            <td>${u.name}</td>
-            <td>${u.username}</td>
-            <td>${u.email}</td>
-            <td>${u.address.city}</td>
-          </tr>`;
+    request.onreadystatechange = () => {
+      if (request.readyState === 4) {
+        if (request.status <= 400) {
+          const data = JSON.parse(request.responseText);
+          resolve(data);
+        } else {
+          reject(new Error('Error code: ' + request.status));
         }
       }
-    }
-  };
+    };
 
-  request.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    request.open('GET', url);
+    request.send();
+  });
+}
 
-  request.send();
+async function showAllusers() {
+  try {
+    const users = await getJsonAsync(
+      'https://jsonplaceholder.typicode.com/users',
+    );
+    displayUsers(users);
+  } catch (err) {
+    alert(err.message);
+  }
 }
 
 function onWindowLoad() {
   const btn1 = document.querySelector('#btn1');
-  btn1.onclick = getUsers;
+  btn1.onclick = showAllusers;
 }
 
 window.onload = onWindowLoad;
